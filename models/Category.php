92489ac2e68aaf7003ed9f8204cc641920cfda78
Gaellan <doucet.mari@gmail.com>
2024-02-02 14:00:28 +0100

    adding the category model

---
+++ models/Category.php
@@ -0,0 +1,31 @@
+<?php
+
+class Category
+{
+    private ? int $id = null;
+
+    public function __construct(private string $name)
+    {
+
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
